132af38f0768d2cd75b2cbf4b899e6601007b6a6
Juffaz <38725561+juffaz@users.noreply.github.com>
2021-07-08 18:45:00 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,27 @@
 import telebot
+from telebot import types
 
-bot = telebot.TeleBot("1873101130:AAHmUWdnFxj0weBV56kyNtqc2PtFxgT9i7Q")
 
-@bot.message_handler(commands = ['switch'])
-def switch(message):
-    markup = types.InlineKeyboardMarkup()
-    switch_button = types.InlineKeyboardButton(text='Try', switch_inline_query="Telegram")
-    markup.add(switch_button)
-    bot.send_message(message.chat.id, "Выбрать чат", reply_markup = markup)
+bot = telebot.TeleBot("1873101130:AAHmUWdnFxj0weBV56kyNtqc2PtFxgT9i7QQ")
+chat_id = 16818130
+
+markup = types.ReplyKeyboardMarkup(row_width=2)
+itembtn1 = types.KeyboardButton('a')
+itembtn2 = types.KeyboardButton('v')
+itembtn3 = types.KeyboardButton('d')
+markup.add(itembtn1, itembtn2, itembtn3)
+bot.send_message(chat_id, "Choose one letter:", reply_markup=markup)
+
+# or add KeyboardButton one row at a time:
+markup = types.ReplyKeyboardMarkup()
+itembtna = types.KeyboardButton('a')
+itembtnv = types.KeyboardButton('v')
+itembtnc = types.KeyboardButton('c')
+itembtnd = types.KeyboardButton('d')
+itembtne = types.KeyboardButton('e')
+markup.row(itembtna, itembtnv)
+markup.row(itembtnc, itembtnd, itembtne)
+bot.send_message(chat_id, "Choose one letter:", reply_markup=markup)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
